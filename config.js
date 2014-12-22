ba1a63db7c47487b8f720c4a96fc12d0ce4b8547
Rob Eisenberg <rob@bluespire.com>
2014-12-22 02:09:30 -0500

    fix(package): update dependencies to latest

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ System.config({
     "aurelia-history-browser": "github:aurelia/history-browser@0.1.1",
     "aurelia-loader-systemjs": "github:aurelia/loader-systemjs@0.2.2",
     "aurelia-logging-console": "github:aurelia/logging-console@0.1.1",
-    "aurelia-router": "github:aurelia/router@0.2.0",
+    "aurelia-router": "github:aurelia/router@0.2.1",
     "aurelia-templating": "github:aurelia/templating@0.4.0",
     "aurelia-templating-binding": "github:aurelia/templating-binding@0.4.0",
     "aurelia-templating-resources": "github:aurelia/templating-resources@0.4.0",
-    "aurelia-templating-router": "github:aurelia/templating-router@0.5.1",
+    "aurelia-templating-router": "github:aurelia/templating-router@0.5.2",
     "github:aurelia/binding@0.1.2": {
       "aurelia-metadata": "github:aurelia/metadata@0.1.1",
       "aurelia-task-queue": "github:aurelia/task-queue@0.1.1"
@@ -48,7 +48,7 @@ System.config({
       "es6-shim": "github:paulmillr/es6-shim@0.21.1",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.2"
     },
-    "github:aurelia/router@0.2.0": {
+    "github:aurelia/router@0.2.1": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.1.1",
       "aurelia-history": "github:aurelia/history@0.1.1",
       "aurelia-path": "github:aurelia/path@0.3.0",
@@ -65,11 +65,11 @@ System.config({
       "aurelia-templating": "github:aurelia/templating@0.4.0",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1"
     },
-    "github:aurelia/templating-router@0.5.1": {
+    "github:aurelia/templating-router@0.5.2": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.1.1",
       "aurelia-metadata": "github:aurelia/metadata@0.1.1",
       "aurelia-path": "github:aurelia/path@0.3.0",
-      "aurelia-router": "github:aurelia/router@0.2.0",
+      "aurelia-router": "github:aurelia/router@0.2.1",
       "aurelia-templating": "github:aurelia/templating@0.4.0"
     },
     "github:aurelia/templating@0.4.0": {
